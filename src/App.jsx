fb68a43858ddb69eb68d6de4468411c65431ddcb
dpw1 <diego.boarutto.fortes@gmail.com>
2023-09-10 01:47:26 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React 1231</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} oo
+          count is {count}!
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
