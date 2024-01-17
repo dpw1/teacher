0c05b61bc1a4278c454ba29498afadda8df08d89
dpw1 <diego.boarutto.fortes@gmail.com>
2024-01-17 14:04:32 +0000

    UPD

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,5 +1,7 @@
 export function scrollToElement($el, yOffset = 0) {
-  const y = $el.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  if (typeof window !== "undefined") {
+    const y = $el.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
-  window.scrollTo({ top: y, behavior: "smooth" });
+    window.scrollTo({ top: y, behavior: "smooth" });
+  }
 }
